e1ebdae97e0121b3a10ddfa3c2b41d4b6965dd6a
Eren56 <74675541+Eren56@users.noreply.github.com>
2021-09-30 12:34:30 -0400

    Update algorithms.cpp
    added some new heuristics

--- algorithms.cpp
+++ algorithms.cpp
@@ -27,6 +27,34 @@ PieceColor findWinner(Board board){
     return findWinner(blueCount, orangeCount);
 }
 
+double mobilityHeuristic(Board board, PieceColor maximizingColor){
+    int maximizingColorMoves = size(board.findAllValidMoves(maximizingColor));
+    int minimizingColorMoves = size(board.findAllValidMoves(static_cast<PieceColor>(-(int)maximizingColor)));
+    if(maximizingColorMoves + minimizingColorMoves != 0){
+        int mobilityScore = 100 * (maximizingColorMoves - minimizingColorMoves) / (maximizingColorMoves + minimizingColorMoves);
+        return mobilityScore;
+    }
+    return 0;
+}
+double cornersCapturedHeuristic(Board board, PieceColor color){
+    //Count corners, then do:
+    /*
+    if ( Max Player Corners + Min Player Corners != 0)
+	    Corner Heuristic Value = 100 * (Max Player Corners - Min Player Corners) / (Max Player Corners + Min Player Corners)
+    else
+	    Corner Heuristic Value = 0
+
+    */
+}
+double weightedBoardHeuristic(Board board, PieceColor color){
+    int weightedBoard[64] = {
+        /*
+        X squares - B2, B7, G2, and G7. Placing your disk there early almost certainly gives away the adjacent corner, so your heuristic should give them high negative weight, at least in the first 40 moves
+        C squares - A2, A7, B1, G1, H2, H7, B8, and G8. They offer the opponent access to corners, so their value should be different from that of other squares, at least when the edge has fewer than five disks
+        */
+    };
+}
+
 int quickHeuristc(Board board) {
     map<PieceColor,int> counts = board.get_counts();
     int blueCount = counts.at(PieceColor::BLUE);
