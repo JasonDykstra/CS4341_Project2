b5d955c79fb5eb5a218952081b17921bb536c80d
Travis Thompson <travisthom01@gmail.com>
2021-09-29 21:25:59 -0400

    quick heuristic function

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,12 +1,9 @@
-#include "Board.cpp";
+#include "board.cpp";
+#include "main.cpp";
+
 using namespace std;
 
-
-
-PieceColor findWinner(Board board){
-    map<PieceColor,int> counts = board.get_counts();
-    int blueCount = counts.at(PieceColor::BLUE);
-    int orangeCount = counts.at(PieceColor::ORANGE);
+PieceColor findWinner(int blueCount, int orangeCount) {
     if(blueCount + orangeCount == Board::boardSize){
         if ((blueCount-orangeCount) > 0){
             return PieceColor::BLUE;
@@ -17,7 +14,45 @@ PieceColor findWinner(Board board){
         else{
             return PieceColor::TIE;
         }
-        return PieceColor::NONE;
+    }
+    return PieceColor::NONE;
+}
+
+
+PieceColor findWinner(Board board){
+    map<PieceColor,int> counts = board.get_counts();
+    int blueCount = counts.at(PieceColor::BLUE);
+    int orangeCount = counts.at(PieceColor::ORANGE);
+    
+    return findWinner(blueCount, orangeCount);
+}
+
+int quickHeuristc(Board board) {
+    map<PieceColor,int> counts = board.get_counts();
+    int blueCount = counts.at(PieceColor::BLUE);
+    int orangeCount = counts.at(PieceColor::ORANGE);
+
+    PieceColor winner = findWinner(blueCount, orangeCount);
+
+    //if game not over
+    if(winner == PieceColor::none) {
+        if(orangeCount > blueCount) {
+            return pieceColor == PieceColor.orange; ? 1 : -1;
+        } else if(blueCount > orangeCount){
+            return pieceColor == PieceColor.orange; ? -1 : 1;
+        } else {
+            return 0;
+        }
+
+    } else {
+        //game over
+        if(winner == pieceColor) {
+            return 5;
+        } else if(winner == PieceColor::TIE) {
+            return -INT.IN;
+        } else {
+            return -5;
+        }
     }
 }
 
